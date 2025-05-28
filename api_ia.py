36cef34e6cd9f7abb82f2e893f4f82c0140ca373
levi13 <30730638+levi13@users.noreply.github.com>
2025-05-28 08:04:18 -0300

    Update api_ia.py

--- api_ia.py
+++ api_ia.py
@@ -5,7 +5,7 @@ import google.generativeai as genai
 app = Flask(__name__)
 
 # Chave da API (deixe em variável de ambiente para segurança)
-genai.configure(api_key=os.environ.get("AIzaSyDSIJC26k3lH54DZCFpGYmqa6tqGShEpQo"))
+genai.configure(api_key="AIzaSyDSIJC26k3lH54DZCFpGYmqa6tqGShEpQo")
 
 @app.route('/sugestoes-refeicoes', methods=['POST'])
 def sugestoes_refeicoes():
